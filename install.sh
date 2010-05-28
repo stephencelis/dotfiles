f163a0f4c81e1e06bd0a095630d65296bc78cb5f
Stephen Celis <stephen@stephencelis.com>
2010-05-28 11:17:24 -0500

    Update install script for diffing. Update submodules.

--- install.sh
+++ install.sh
@@ -2,17 +2,25 @@
 
 path=$(pwd -P)
 
-for each in $(echo * | sort | awk '{gsub(/README|install.sh/,"");print}'); do
-  if [[ $each != 'install.sh' ]] && [[ $each != 'README' ]]; then
+for each in $(echo * | awk '{gsub(/README|install.sh/,"");print}')
+do
+  if [[ $each != 'install.sh' ]] && [[ $each != 'README' ]]
+  then
     echo Installing .$each into $HOME...
-    if [[ -f $HOME/.$each ]] || [[ -d $HOME/.$each ]]; then
-      [[ $stdin != 'a' ]] && printf "Exists. Overwrite? [yNaq] " && read stdin
-      [[ $stdin == 'q' ]] && exit
-      if [[ $stdin == 'a' ]] || [[ $stdin == 'y' ]]; then
-        echo 'Overwriting...'
-        rm $HOME/.$each
-        ln -s $path/$each $HOME/.$each
-      fi
+    if [[ -f $HOME/.$each ]] || [[ -d $HOME/.$each ]]
+    then
+      while [[ -z $stdin ]] || [[ $stdin == 'd' ]]
+      do
+        [[ $stdin == 'd' ]] && diff $path/$each $HOME/.$each
+        [[ $stdin != 'a' ]] && printf "Exists. Overwrite? [yNdaq] " && read stdin
+        [[ $stdin == 'q' ]] && exit
+        if [[ $stdin == 'a' ]] || [[ $stdin == 'y' ]]
+        then
+          echo 'Overwriting...'
+          ln -fs $path/$each $HOME/.$each
+        fi
+      done
+      stdin=''
     else
       ln -s $path/$each $HOME/.$each
     fi
