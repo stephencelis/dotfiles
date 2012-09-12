692f2bd591e6d288626823da626236e2fcc4124b
Stephen Celis <stephen@stephencelis.com>
2012-09-11 22:04:24 -0700

    Zsh updates
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- oh-my-zsh/custom/plugins/stephencelis/stephencelis.plugin.zsh
+++ oh-my-zsh/custom/plugins/stephencelis/stephencelis.plugin.zsh
@@ -1,15 +1,48 @@
-if [[ -f $HOME/.zsh-local ]]
-then
-  source $HOME/.zsh-local
-fi
+unsetopt correct_all
+export PATH="$(dirname $0)/bin:$PATH"
 
+# Glob search.
+bindkey '^R' history-incremental-pattern-search-backward
+bindkey '^S' history-incremental-pattern-search-forward
+
+# Aliases.
+alias which="builtin which -a"
+alias ssh-add-all="ssh-add ~/.ssh/*id_rsa"
+
+# Quickly toggle your editor with ^Z.
+foreground-editor() { fg $EDITOR 2>/dev/null }
+zle -N foreground-editor
+bindkey '^Z' foreground-editor
+
+# ~Code completion.
 Code="$HOME/Documents/Code"
 c() { cd $Code; for match in $*; do eval cd "$match*(/,@[1])"; done }
 _c() { _files -W $Code -/ }
 compdef _c c
 
-unsetopt correct_all
+# http://blog.plenz.com/2012-01/zsh-complete-words-from-tmux-pane.html
+_tmux_pane_words() {
+  local expl
+  local -a w
+  if test -z "$TMUX_PANE"
+  then
+    return 1
+  fi
+  w=( ${(u)=$(tmux capture-pane \; show-buffer \; delete-buffer)} )
+  _wanted values expl 'words from current tmux pane' compadd -a w
+}
+zle -C tmux-pane-words-prefix   complete-word _generic
+zle -C tmux-pane-words-anywhere complete-word _generic
+bindkey '^Xt' tmux-pane-words-prefix
+bindkey '^X^X' tmux-pane-words-anywhere
+zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' completer \
+  _tmux_pane_words
+zstyle ':completion:tmux-pane-words-(prefix|anywhere):*' ignore-line \
+  current
+zstyle ':completion:tmux-pane-words-anywhere:*' matcher-list \
+  'b:=* m:{A-Za-z}={a-zA-Z}'
 
-alias which="builtin which -a"
-
-export PATH="$(dirname $0)/bin:$PATH"
+if test -f $HOME/.zsh-local
+then
+  source $HOME/.zsh-local
+fi
--- zshenv
+++ zshenv
@@ -4,19 +4,13 @@ PATH="$HOME/.local/bin:$PATH"
 PATH="/usr/local/bin:/usr/local/sbin:/usr/local/share/python:$PATH"
 # jsc.
 PATH="$PATH:/System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources"
-# as.
+# ActionScript.
 PATH="$PATH:/$HOME/Library/Developer/flex_sdk_4/bin"
 export PATH
 
-if [[ -x /Applications/MacVim.app/Contents/MacOS/Vim ]]
-then
-  EDITOR="/Applications/MacVim.app/Contents/MacOS/Vim"
-elif [[ -x $HOME/Applications/MacVim.app/Contents/MacOS/Vim ]]
-then
-  EDITOR="$HOME/Applications/MacVim.app/Contents/MacOS/Vim"
-else
-  EDITOR="vim"
-fi
+# MacVim.
+EDITOR=$(command -v {~,}/Applications/MacVim.app/Contents/MacOS/Vim | head -1)
+EDITOR=${EDITOR:=vim}
 export EDITOR
 export VISUAL=$EDITOR
 alias vim=$EDITOR
