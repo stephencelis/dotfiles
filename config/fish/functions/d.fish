d4e10c69793f0d32cb54508a9148a2e9d51f09e3
Stephen Celis <stephen@stephencelis.com>
2015-04-12 19:01:23 -0400

    Print error when d() fails
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- config/fish/functions/d.fish
+++ config/fish/functions/d.fish
@@ -2,5 +2,9 @@ function d --description 'Like z, but d'
     cd $DEVELOPER
     for dir in $argv
         cd $dir*/
+        or begin
+            echo "fish: No matches for wildcard '$dir*/'." >&2
+            return 1
+        end
     end
 end
