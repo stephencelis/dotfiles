3b07ed8d850cf65c4ecaca1809e4082550d0375c
Stephen Celis <stephen@stephencelis.com>
2015-04-13 12:35:22 -0400

    Re-tab pre-generated fish_prompt.fish
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,24 +1,24 @@
 function fish_prompt --description 'Write out the prompt'
 
-	set -l last_status $status
+    set -l last_status $status
 
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
-	end
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
 
-	# PWD
-	set_color $fish_color_cwd
-	echo -n (prompt_pwd)
-	set_color normal
+    # PWD
+    set_color $fish_color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
 
-	printf '%s ' (__fish_git_prompt ' %s')
+    printf '%s ' (__fish_git_prompt ' %s')
 
-	if not test $last_status -eq 0
-		set_color $fish_color_error
-	end
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
 
-	echo -n '$ '
+    echo -n '$ '
 
-	set_color normal
+    set_color normal
 
 end
