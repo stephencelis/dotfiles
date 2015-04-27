a1cee1c628bed62791ce27bc340e68e2e9fb8c86
Stephen Celis <stephen@stephencelis.com>
2015-04-27 09:00:59 -0400

    Pass xcopen arguments to open -a Xcode
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- config/fish/functions/xcopen.fish
+++ config/fish/functions/xcopen.fish
@@ -1,4 +1,9 @@
 function xcopen --description 'Open the nearest Xcode workspace or project'
+    if count $argv > /dev/null
+        open -a Xcode $argv
+        return
+    end
+
     pushd *.{xcworkspace,xcodeproj}
     and open .
     and popd
