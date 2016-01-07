84ee0b998d95e47c5e1e381f064f978b2559b61b
Stephen Celis <stephen@stephencelis.com>
2016-01-07 15:46:28 -0500

    Abbreviate hours
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -2,10 +2,13 @@ function fish_prompt --description 'Write out the prompt'
 
     if test "$CMD_DURATION" -gt '100'
         set_color $fish_color_error
-        if test "$CMD_DURATION" -gt '60000'
+
+        if test "$CMD_DURATION" -lt '60000'
+            printf '%.1fs ' (awk "BEGIN { print $CMD_DURATION / 1000 }")
+        else if test "$CMD_DURATION" -lt '3600000'
             printf '%.1fm ' (awk "BEGIN { print $CMD_DURATION / 60000 }")
         else
-            printf '%.1fs ' (awk "BEGIN { print $CMD_DURATION / 1000 }")
+            printf '%.1fh ' (awk "BEGIN { print $CMD_DURATION / 3600000 }")
         end
         set_color normal
     end
