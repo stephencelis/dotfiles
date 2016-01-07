3d315be5ff20ba384b6557a85a76bf65e8be179d
Stephen Celis <stephen@stephencelis.com>
2016-01-07 10:41:29 -0500

    Add (crudely) the last command's duration when over 100ms
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,5 +1,15 @@
 function fish_prompt --description 'Write out the prompt'
 
+    if test "$CMD_DURATION" -gt '100'
+        set_color $fish_color_error
+        if test "$CMD_DURATION" -gt '60000'
+            printf '%.1fm ' (awk "BEGIN { print $CMD_DURATION / 60000 }")
+        else
+            printf '%.1fs ' (awk "BEGIN { print $CMD_DURATION / 1000 }")
+        end
+        set_color normal
+    end
+
     set -l last_status $status
 
     if not set -q __fish_prompt_normal
